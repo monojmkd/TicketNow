c23a000de7c37939f858031e4e4fbc9aaed7437f
monojmkd <monoj.mkd@hotmail.com>
2026-03-17 17:07:43 +0530

    fix: db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,17 @@
 require("dns").setDefaultResultOrder("ipv4first");
 
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
+
+// 🔥 FORCE SSL at driver level
+pg.defaults.ssl = {
+  require: true,
+  rejectUnauthorized: false,
+};
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 });
 
 module.exports = sequelize;
